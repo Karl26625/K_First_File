8074ca78be9446029facbf5ffd7f547f94d2c359
Karl26625 <62194262+Karl26625@users.noreply.github.com>
2020-10-06 16:28:21 +1100

    Unity File

---
+++ Assets/CamerP2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CamerP2 : MonoBehaviour
+{
+
+    public Material mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
+    }
+
+    // Update is called once per frame
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (mat != null)
+        {
+            Graphics.Blit(source, destination, mat);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+}
